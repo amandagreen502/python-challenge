824aa23964182e1f9e7383bdc3550dde84855ebd
Amanda Green <amandagreen502@gmail.com>
2019-06-09 19:19:18 -0400

    adding code to pybank for avg change and greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,5 @@
 # import os & cv file
+import numpy as nb
 import os
 import csv
 csvpath = os.path.join('budget_data.csv')
@@ -9,7 +10,7 @@ tot_revenue = 0
 prev_revenue = 0
 month_of_change = []
 revenue_change_list = []
-
+biggest_decr = 0
 
 # read csv 
 with open(csvpath, newline='') as csvfile:
@@ -20,12 +21,25 @@ with open(csvpath, newline='') as csvfile:
     # Read each row of data after the header
     for row in csvreader:
 
- #Total Number of Months
+ #Total Number of Months Calculation
         total_months += 1
 #Profit/Losses Total Revenue Calculation
-        tot_revenue += int(row['Profit/Losses'])  
+        tot_revenue += int(row['Profit/Losses']) 
+
+#Calulate average change 
+prev_change = int(row["Profit/Losses"]) - prev_revenue
+prev_revenue = int(row["Profit/Losses"])
+revenue_change_list = revenue_change_list + [rev_change]
+month_of_change = month_of_change + [row["Date"]]
 
 
+# The greatest decrease in losses (date and amount) over the entire period
+
+biggest_decr = np.min(int(row['Profit/Losses'])) 
+
+#The greatest increase in profits (date and amount) over the entire period
+
+biggest_decr = np.max(int(row['Profit/Losses']))
       
 
 
