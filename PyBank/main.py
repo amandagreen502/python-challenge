0686d980d700411cbbfb1349933f2c35e02baf92
Amanda Green <amandagreen502@gmail.com>
2019-06-09 18:55:13 -0400

    adding code to pybank for total months and total revenue in profit and losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,14 @@ import os
 import csv
 csvpath = os.path.join('budget_data.csv')
 
+# Define Variables
+total_months = 0
+tot_revenue = 0
+prev_revenue = 0
+month_of_change = []
+revenue_change_list = []
+
+
 # read csv 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -11,11 +19,15 @@ with open(csvpath, newline='') as csvfile:
     
     # Read each row of data after the header
     for row in csvreader:
-   
 
-           
+ #Total Number of Months
+        total_months += 1
+#Profit/Losses Total Revenue Calculation
+        tot_revenue += int(row['Profit/Losses'])  
+
 
       
 
 
-           
+
+file_output = "results.txt"
