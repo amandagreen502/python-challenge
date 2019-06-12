24ff83963dfebec8cd365471089eb122742a40af
Amanda Green <amandagreen502@gmail.com>
2019-06-12 01:06:10 -0400

    Add code for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,18 @@
-print("test")
+# import os & cv file
+import os
+import csv
+
+#define variables
+candidates = []
+num_votes = 0
+vote_counts = []
+
+
+# read csv 
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # Read header 
+    csv_header = next(csvreader)
+    
+    # Read each row of data after the header
+    for row in csvreader:
